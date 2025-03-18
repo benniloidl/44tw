8a74cd8b7b70747f12a146127f34091e12c9eb2f
benniloidl <benni@loidlnetz.de>
2025-03-18 20:16:47 +0100

    moving logo

--- app/page.tsx
+++ app/page.tsx
@@ -73,14 +73,6 @@ export default function Home() {
   };
 
   const waitingElement = <>
-    <Image
-      className={styles.logo}
-      src="/next.svg"
-      alt="Next.js logo"
-      width={180}
-      height={38}
-      priority
-    />
     <h1>
       Waiting for opponent
     </h1>
@@ -121,6 +113,14 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
+        <Image
+          className={styles.logo}
+          src="/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
         { connectionStatus === "waiting"
           ? waitingElement
           : <Game pitch={ pitch } turn={ turn } onColClick={ onColClick } />
