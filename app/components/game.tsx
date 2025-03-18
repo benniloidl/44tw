ff24689715b0267a2e624ec66560224910b188ad
benniloidl <benni@loidlnetz.de>
2025-03-18 19:56:12 +0100

    Improving game styles

--- app/components/game.tsx
+++ app/components/game.tsx
@@ -9,22 +9,13 @@ export default function Game(props: { pitch: PitchCellValue[][], turn: boolean, 
         <h1>Game{ props.turn && " (Your turn)" }</h1>
         <div className="game_pitch">
             { Array.from({ length: colCount }, (_, colIndex) => (
-                <div className="game_pitch_col" data-col={colIndex} onClick={props.onColClick} key={colIndex}>
-                    { Array.from({ length: rowCount }, (_, cellIndex) => {
-                        let cellOwner;
-                        switch (props.pitch[cellIndex][colIndex]) {
-                            case PitchCellValue.OWN:
-                                cellOwner = "game_pitch_cell_own";
-                                break;
-                            case PitchCellValue.OTHER:
-                                cellOwner = "game_pitch_cell_other";
-                                break;
-                            default:
-                                cellOwner = "game_pitch_cell_none";
-                        }
-
-                        return <div className={`game_pitch_cell ${cellOwner}`} key={cellIndex} />
-                    }) }
+                <div className="game_pitch_col" data-col={ colIndex } onClick={ props.onColClick } key={ colIndex }>
+                    { Array.from({ length: rowCount }, (_, cellIndex) => (
+                        <div className="game_pitch_cell" key={ cellIndex }>
+                            { props.pitch[cellIndex][colIndex] === PitchCellValue.OWN && <div className="game_pitch_cell_own" /> }
+                            { props.pitch[cellIndex][colIndex] === PitchCellValue.OTHER && <div className="game_pitch_cell_other" /> }
+                        </div>
+                    )) }
                 </div>
             )) }
         </div>
